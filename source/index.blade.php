46dc37d8fa289521eca66a0bda3284ed447ff205
Michael Faas <mfaas@echocyber.io>
2026-04-14 15:38:51 -0400

    fix: correct photo filename in About section (Michael_Faas.jpg)

--- source/index.blade.php
+++ source/index.blade.php
@@ -104,7 +104,7 @@ description: Fractional CTO & CISO for growth-stage companies navigating technol
         <div class="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-16 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-5">
             {{-- Photo --}}
             <div class="lg:col-span-2">
-                <img src="/assets/img/mike-faas-hero.jpg"
+                <img src="/assets/img/Michael_Faas.jpg"
                      alt="Mike Faas"
                      class="w-full rounded-2xl object-cover object-top aspect-[3/4] ring-1 ring-echo-700/30">
             </div>
